5267f7ea830fafdd022f90b19d5fa0a577bb8ba8
saifur111 <ssaaiiffuurr240@gmail.com>
2024-03-07 17:06:39 +0600

    setup post page

---
+++ src/components/posts/PostList.jsx
@@ -0,0 +1,8 @@
+import PostCard from "./PostCard";
+const PostList = ({ posts }) => {
+    return (
+        !!posts && posts.map((post) => <PostCard key={post.id} post={post} />)
+    );
+};
+
+export default PostList;
