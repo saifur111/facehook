5366755044e421f1b43c02706e0ce49533299229
saifur111 <ssaaiiffuurr240@gmail.com>
2024-03-07 12:16:52 +0600

    jwt token

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { Routes,Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
-import ProfilePage from "./pages/ProfilePage"
-import RegistrationPage from "./pages/RegistrationPage"
-import NotFoundPage from "./pages/NotFoundPage"
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import ProfilePage from './pages/ProfilePage';
+import RegistrationPage from './pages/RegistrationPage';
+
+import PrivateRoutes from './routes/PrivateRoutes';
+
 function App() {
 
   return (
     <>
       <Routes>
-        <Route element={<HomePage/>} path='/' exact/>
-        <Route element={<LoginPage/>} path='/login'/>
-        <Route element={<ProfilePage/>} path='/me'/>
-        <Route element={<RegistrationPage/>} path='/register'/>
-        <Route element={<NotFoundPage/>} path='*'/>
-      </Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<HomePage />} path="/" exact />
+          <Route element={<ProfilePage />} path="/me" />
+        </Route>
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<RegistrationPage />} path="/register" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes> 
     </>
   )
 }
