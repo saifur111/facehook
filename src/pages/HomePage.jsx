df1d01e147cf9020d1728057cb65fda9c7b1f957
saifur111 <ssaaiiffuurr240@gmail.com>
2024-03-07 11:05:36 +0600

    start auth with context

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,18 @@
-import Header from "../components/common/Header"
 
+import { useAuth } from '../hooks/useAuth'
+
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
+  const {auth} = useAuth();
+  console.log(auth);
+
   return (
-    <>
-      <Header />
-    </>
+    <div>
+      <p>HomePage</p>
+      <Link to="/me">Go to Profile Page</Link>
+    </div>
   )
-} 
+}
 
 export default HomePage
